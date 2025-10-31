ef5a6ed4d1d0dd43e543fa35e186f04521cf66ab
joncorv <joncorv@gmail.com>
2025-10-31 13:58:48 -0700

    flake.nix updated

--- flake.nix
+++ flake.nix
@@ -66,13 +66,16 @@
           pname = "filera";
           version = "0.4.2";
 
-          # Whole repo, but set the source root correctly
+          # Include whole repo
           src = pkgs.lib.cleanSource ./.;
-          sourceRoot = "src-tauri";
 
           cargoLock.lockFile = ./src-tauri/Cargo.lock;
 
-          nativeBuildInputs = with pkgs; [ pkg-config ];
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            nodejs
+            yarn
+          ];
           buildInputs = with pkgs; [
             dbus.dev
             at-spi2-atk
@@ -91,8 +94,34 @@
             gsettings-desktop-schemas
           ];
 
+          # Tell nix to unpack and cd into src-tauri manually
+          preUnpack = ''
+            echo ">>> Using manual unpack for src-tauri"
+          '';
+          unpackPhase = ''
+            mkdir source
+            tar -xf $src --strip-components=1 -C source
+            cd source/src-tauri
+            sourceRoot=$(pwd)
+          '';
+
           preBuild = ''
             export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS"
+          '';
+
+          # Optional: package frontend before Rust build
+          buildPhase = ''
+            echo ">>> Building frontend"
+            cd ..
+            yarn install --frozen-lockfile
+            yarn build
+            cd src-tauri
+            cargo build --release --frozen
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp target/release/filera $out/bin/
           '';
         };
 
