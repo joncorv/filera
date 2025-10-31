12ee46aaff19f00c41b8f27450a59b039e7e8b15
joncorv <joncorv@gmail.com>
2025-10-31 12:32:26 -0700

    updated flake to properly look at tags from github actions

--- flake.nix
+++ flake.nix
@@ -16,6 +16,14 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        # Map Nix system to Tauri architecture naming
+        tauriArch =
+          {
+            "x86_64-linux" = "x64";
+            "aarch64-linux" = "arm64";
+          }
+          .${system} or (throw "Unsupported system: ${system}");
       in
       {
         packages.default = pkgs.stdenv.mkDerivation rec {
@@ -23,7 +31,7 @@
           version = "0.4.2";
 
           src = pkgs.fetchurl {
-            url = "https://github.com/joncorv/filera/releases/download/v${version}/filera_${version}_x64.app.tar.gz";
+            url = "https://github.com/joncorv/filera/releases/download/filera-v${version}/filera_${version}_${tauriArch}.tar.gz";
             hash = ""; # Get this from first build error
           };
 
@@ -45,7 +53,7 @@
             libsoup_3
             pango
             harfbuzz
-            # Additional runtime dependencies from your shell.nix
+            # Additional runtime dependencies
             at-spi2-atk
             atkmm
             fontconfig
@@ -77,7 +85,10 @@
           meta = with pkgs.lib; {
             description = "A powerful, cross-platform batch file renaming tool";
             homepage = "https://github.com/joncorv/filera";
-            platforms = platforms.linux;
+            platforms = [
+              "x86_64-linux"
+              "aarch64-linux"
+            ];
           };
         };
       }
