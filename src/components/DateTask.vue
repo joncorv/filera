5afbd1a6a752d32eb7d2118abdab4b68fd3f945d
joncorv <joncorv@gmail.com>
2026-01-29 13:05:56 -0800

    DateTask moved from Dropdown -> Select

--- src/components/DateTask.vue
+++ src/components/DateTask.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import InputText from "primevue/inputtext";
 import FloatLabel from "primevue/floatlabel";
-import Dropdown from "primevue/dropdown";
 import ToggleButton from "primevue/togglebutton";
+import Select from "primevue/select";
 
 defineProps<{
     task: {
@@ -57,40 +57,40 @@ const emit = defineEmits<{
         <div class="flex flex-row gap-3 items-center">
 
             <!-- === Date Dropdown === -->
-            <Dropdown v-model="task.Date.year" :options="[
+            <Select v-model="task.Date.year" :options="[
                 { label: 'Year YYYY', value: 0 },
                 { label: 'Year YY', value: 1 },
                 { label: 'Year Off', value: 2 },
-            ]" optionLabel="label" optionValue="value" placeholder="" size="small"
-                class="flex-1" @change="emit('update')" />
+            ]" optionLabel="label" optionValue="value" placeholder="" size="small" class="flex-1"
+                @change="emit('update')" />
 
             <!-- === Month Dropdown === -->
-            <Dropdown v-model="task.Date.month" :options="[
+            <Select v-model="task.Date.month" :options="[
                 { label: 'Month On', value: true },
                 { label: 'Month Off', value: false },
-            ]" optionLabel="label" optionValue="value" placeholder="" size="small"
-                class="flex-1" @change="emit('update')" />
+            ]" optionLabel="label" optionValue="value" placeholder="" size="small" class="flex-1"
+                @change="emit('update')" />
 
             <!-- === Day Dropdown === -->
-            <Dropdown v-model="task.Date.day" :options="[
+            <Select v-model="task.Date.day" :options="[
                 { label: 'Day On', value: true },
                 { label: 'Day Off', value: false },
-            ]" optionLabel="label" optionValue="value" placeholder="" size="small"
-                class="flex-1" @change="emit('update')" />
+            ]" optionLabel="label" optionValue="value" placeholder="" size="small" class="flex-1"
+                @change="emit('update')" />
 
             <!-- === Separator === -->
             <div class="flex">
                 <FloatLabel variant="on">
-                    <InputText class="w-21" v-model="task.Date.separator"
-                        :id="`separator-${index}`" size="small" @input="emit('update')" />
+                    <InputText class="w-21" v-model="task.Date.separator" :id="`separator-${index}`" size="small"
+                        @input="emit('update')" />
                     <label for="`separator-${index}`">Separator</label>
                 </FloatLabel>
             </div>
 
             <!-- === Position at Start or End === -->
             <div class="flex-1">
-                <ToggleButton v-model="task.Date.at_start" onLabel="@ Start"
-                    offLabel="@ End" size="small" @change="emit('update')" />
+                <ToggleButton v-model="task.Date.at_start" onLabel="@ Start" offLabel="@ End" size="small"
+                    @change="emit('update')" />
             </div>
 
         </div>
