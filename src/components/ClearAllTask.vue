2e217b7b7d41807cd818bfc43540f7716e67a919
joncorv <joncorv@gmail.com>
2026-01-29 11:07:38 -0800

    ClearAllTask component created

---
+++ src/components/ClearAllTask.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+defineProps<{
+    index: number;
+    isFirst: boolean;
+    isLast: boolean;
+}>();
+
+const emit = defineEmits<{
+    (e: 'delete', index: number): void;
+    (e: 'move-up', index: number): void;
+    (e: 'move-down', index: number): void;
+}>();
+</script>
+
+<template>
+    <div class="task-container">
+        <!-- === Title and Description === -->
+        <div class="flex flex-row items-center justify-between mb-1">
+            <div class="flex flex-row items-center gap-2">
+                <span class="pi pi-eraser text-textprimary"></span>
+                <h4 class="text-sm font-semibold text-textprimary m-0">Clear All</h4>
+                <p class="text-xs text-textprimary m-0">Remove all text from file names</p>
+            </div>
+            <!-- === Dummy Spacer === -->
+            <div class="flex-1"></div>
+
+            <!-- === Close Button === -->
+            <i class="pi pi-angle-up text-textprimary hover:cursor-pointer text-sm mr-1"
+                :class="{ 'opacity-30': isFirst }" @click="emit('move-up', index)"></i>
+            <i class="pi pi-angle-down text-textprimary hover:cursor-pointer text-sm mr-1"
+                :class="{ 'opacity-30': isLast }" @click="emit('move-down', index)"></i>
+            <!-- === Close Button === -->
+            <div class="flex items-center" @click="emit('delete', index)">
+                <i class="pi pi-times hover:cursor-pointer text-sm text-textprimary hover:text-red-500 transition-colors"
+                    style="font-size: 0.9rem"></i>
+            </div>
+        </div>
+    </div>
+</template>
