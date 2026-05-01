4c5eb45cea9d866517ca92bddf4861e5d563f18e
Jonathan E. Corriveau <joncorv@gmail.com>
2026-04-30 19:37:47 -0700

    user_filestatus_shift_click is finished

--- src-tauri/src/user_filestatus.rs
+++ src-tauri/src/user_filestatus.rs
@@ -102,6 +102,7 @@ pub fn user_filestatus_shift_click(index: usize, state: State<'_, Mutex<AppState
     {
         let mut state = state.lock().unwrap();
         let selected_filestatuses = state.selected_filestatuses.clone();
+        let last_selected_filestatus = state.last_selected_filestatus.clone();
 
         if selected_filestatuses.is_none() {
             let mut new_hash: HashSet<usize> = HashSet::new();
@@ -115,28 +116,20 @@ pub fn user_filestatus_shift_click(index: usize, state: State<'_, Mutex<AppState
                 eprintln!("errror: can't set filestatus.selected on index: {index:?}");
             }
         } else if let Some(sf) = selected_filestatuses {
-            if sf.contains(&index) {
-                if let Some(filestatus) = state.file_statuses.get_mut(index) {
-                    filestatus.selected = false;
-                    state.last_selected_filestatus = None;
-
+            if let Some(ls) = last_selected_filestatus {
+                let start = std::cmp::min(ls, index);
+                let end = std::cmp::max(ls, index);
+                (start..=end).for_each(|i| {
                     if let Some(ssf) = &mut state.selected_filestatuses {
-                        ssf.remove(&index);
+                        ssf.insert(i);
                     }
-                } else {
-                    eprintln!("errror: can't set filestatus.selected on index: {index:?}");
-                }
-            } else {
-                if let Some(filestatus) = state.file_statuses.get_mut(index) {
-                    filestatus.selected = true;
-                    state.last_selected_filestatus = Some(index);
 
-                    if let Some(ssf) = &mut state.selected_filestatuses {
-                        ssf.insert(index);
+                    if let Some(filestatus) = state.file_statuses.get_mut(i) {
+                        filestatus.selected = true;
+                    } else {
+                        eprintln!("errror: can't set filestatus.selected on index: {i:?}");
                     }
-                } else {
-                    eprintln!("errror: can't set filestatus.selected on index: {index:?}");
-                }
+                });
             }
         }
     }
@@ -148,30 +141,3 @@ pub fn user_filestatus_selection_clear() {}
 
 #[tauri::command]
 pub fn user_filestatus_selection_delete() {}
-
-// #[tauri::command]
-// pub fn apply_selections_to_filestatuses(state: &State<'_, Mutex<AppState>>) {
-//     let mut state = state.lock().unwrap();
-//     let selected = state.selected_filestatuses.clone();
-//
-//     if let Some(selected) = selected {
-//         if let Some(file_statuses) = &mut state.file_statuses {
-//             selected.iter().for_each(|selected_index| {
-//                 if let Some(foo) = file_statuses.get_mut(*selected_index) {
-//                     foo.selected = true;
-//                 }
-//             })
-//         }
-//     }
-// }
-
-// pub fn user_open_files(file_names: Vec<String>, state: State<'_, Mutex<AppState>>) -> Vec<FileStatus> {
-//     solve_duplicates(file_names, &state);
-//     sort_file_names(&state);
-//     convert_file_names_to_working_files(&state);
-//     process_tasks_on_working_files(&state);
-//     resolve_workingfile_duplicates(&state);
-//     convert_working_files_to_file_status(&state);
-//     apply_selections_to_filestatuses(&state);
-//     apply_search_to_filestatuses(&state)
-// }
